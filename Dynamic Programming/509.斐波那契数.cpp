bc5fd78c035e2d491de1f66b76813b0a99189400
lvlanbing <ll_bing@outlook.com>
2022-04-06 11:23:33 +0800

    main

--- Dynamic Programming/509.斐波那契数.cpp
+++ Dynamic Programming/509.斐波那契数.cpp
@@ -9,6 +9,7 @@ class Solution {
 public:
     int fib(int n) {
         if(n == 0) return 0;
+        // a，b分别记录前面第二个和第一个状态的值
         int a = 0, b = 1, c = 1;
 
         for(int i = 1; i < n; ++i) {
